636bfd64a2d6c7979626ae3e23508f25c97ea815
Sanskar Thamke <59289782+sanskar46@users.noreply.github.com>
2024-02-17 07:38:03 +0000

    datatypes done

---
+++ 3_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert("Dadya"); //only run on console and it gives a pop up output with the text "Dadya"
+
+
+// Code Readebility is Very Important that should be HIGH
+
+let name = "Sanskar"  //string
+let age = 24   //number
+let isLoggedIn = false //boolean
+
+/*
+ DATATYPES
+
+ -----Primitive datatypes-----
+
+ number => 2 to power 53
+ bigint
+ string => ""
+ boolean => true/false
+ null => standalone value  - empty
+ undefined => if the value is not defined 
+ symbol => to find uniqueness (react)
+
+ -----Non - Primitive datatypes-----
+
+ object
+*/
+
+console.log(typeof "Sanskar"); //string
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined - special type
